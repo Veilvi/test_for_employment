81d68a197067f97ad537ee2247644bb216b4474e
Sergey <s.v.ivliev@outlook.com>
2023-11-20 22:52:20 +0300

    Initial commit

---
+++ Test/Tests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using test_for_employment;
+using Xunit;
+
+namespace Test
+{
+    public class Tests
+    {
+      /*  [Fact]
+        public void Test1()
+        {
+            var circle = new Circle((0, 0), (3, 0));
+            Assert.Equal((decimal)28.2743338823081, (decimal)circle.GetFigureArea());
+           
+        }
+        */
+        public static IEnumerable<object[]> CircleTestData() {
+            yield return new object[] { (0.0,0.0), (0.0,3.0), 28.2743338823081};
+            yield return new object[] { (0.0,0.0), (0.0,4.0), 50.2654824574367};
+            yield return new object[] { (0.0,0.0), (0.0,5.0), 78.5398163397448};
+            yield return new object[] { (0.0,0.0), (0.0,6.0), 113.097335529233};
+            yield return new object[] { (0.0,0.0), (0.0,7.0), 153.938040025900};
+            yield return new object[] { (0.0,0.0), (0.0,8.0), 201.061929829747};
+            yield return new object[] { (0.0,0.0), (0.0,9.0), 254.469004940773};
+            yield return new object[] { (0.0,0.0), (0.0,10.0),314.159265358979};
+        }
+
+        public static IEnumerable<object> TriangleTestData()
+        {
+            yield return new object[] { (0.0,0.0), new List<(double, double)>{(1,1),(-2,4),(-2,-2)}, 9};
+            yield return new object[] { (0.0,0.0), new List<(double, double)>{(0,0),(-1,-4),(5,5)}, 7.5};
+            yield return new object[] { (0.0,0.0), new List<(double, double)>{(2,5),(-3,2),(-10,-10)}, 19.5};
+            yield return new object[] { (0.0,0.0), new List<(double, double)>{(-1,-1),(-10,-6),(-8,-8)}, 14};
+        }
+
+        public static IEnumerable<object> IsTriangleRightTestData()
+        {
+            yield return new object[] { (0.0,0.0), new List<(double, double)>{(1,1),(3,1),(1,5)}, false};
+        }
+
+        public static IEnumerable<object> PolygonTestData()
+        {
+            yield return new object[] { (0.0,0.0), new List<(double, double)>{(1,1),(-2,4),(-2,-2),(2,2)}, 9};
+            yield return new object[] { (0.0,0.0), new List<(double, double)>{(1,1),(-2,4),(-2,-2),(2,2),(5,5)}, 9};
+            yield return new object[] { (0.0,0.0), new List<(double, double)>{(1,1),(-2,4),(-2,-2),(-1,-1),(3,3),(-3,2),(6,6)}, 1.5};
+        }
+        
+        [Theory]
+        [MemberData(nameof(CircleTestData))]
+      
+        public void CircleTheory((double, double) center, (double, double) radius, double expected)
+        {
+            var circle = new Circle(center, radius);
+            Assert.Equal((decimal)expected, (decimal)circle.GetFigureArea());
+        }
+
+        [Theory]
+        [MemberData(nameof(TriangleTestData))]
+        public void TriangleTheory((double, double) center, List<(double, double)> vertices, double expected)
+        {
+            var triangle= new Triangle(center,vertices);
+            Assert.Equal((decimal)expected, (decimal)triangle.GetFigureArea());
+        }
+
+        [Theory]
+        [MemberData(nameof(IsTriangleRightTestData))]
+        public void IsTriangleRightTheory((double, double) center, List<(double, double)> vertices, bool expected)
+        {
+            var triangle= new Triangle(center,vertices);
+            Assert.Equal(expected, triangle.IsTriangleRight());
+        }
+        
+        [Theory]
+        [MemberData(nameof(PolygonTestData))]
+        public void PolygonTheory((double, double) center, List<(double, double)> vertices, double expected)
+        {
+            var polygon= new Polygon(center,vertices);
+            Assert.Equal((decimal)expected, (decimal)polygon.GetFigureArea());
+        }
+    }
+}
---
+++ test_for_employment/Figure.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace test_for_employment
+{
+    public abstract class FigureArea
+    {
+        protected (double x, double y) Center;
+        protected List<(double x, double y)> Vertices= new List<(double x, double y)>();
+        public abstract double GetFigureArea();
+        public abstract void ToCenter();
+    }
+
+    public class Circle : FigureArea
+    {
+        public Circle((double, double) center, (double, double) radius)
+        {
+            Center = center;
+            Vertices.Add(radius);
+        }
+
+        public override double GetFigureArea()
+        {
+           var rad=Math.Sqrt(Math.Pow(Vertices.First().y - Center.y, 2) + Math.Pow(Vertices.First().x - Center.x, 2));
+           return  Math.PI * Math.Pow(rad, 2);
+        }
+
+        public override void ToCenter()
+        {
+            var valueTuple = Vertices.First();
+            valueTuple.x = +Center.x;
+            valueTuple.y = +Center.y;
+            Vertices[0] = valueTuple;
+            Center = (0, 0);
+        }
+    }
+
+    public class Triangle : FigureArea
+    {
+        public Triangle((double, double) center, List<(double, double)> vertices)
+        {
+            Center = center;
+            for (int i = 0; i < 3; i++)
+            {
+                Vertices.Add(vertices[i]);
+            }
+        }
+        public override double GetFigureArea()
+        {
+            double sum=0;
+            for (var i = 0; i < Vertices.Count-1; i++)
+            {
+                sum += Vertices[i].x * Vertices[i + 1].y - Vertices[i].y * Vertices[i + 1].x;
+            }
+            sum += Vertices[Vertices.Count-1].x * Vertices[0].y - Vertices[Vertices.Count-1].y * Vertices[0].x;
+            return Math.Abs(sum)/2 ;
+        }
+
+        public override void ToCenter()
+        {
+            for (var i = 0; i < Vertices.Count-1; i++)
+            {
+                var valueTuple = Vertices[i];
+                valueTuple.x = +Center.x;
+                valueTuple.y = +Center.y;
+                Vertices[i] = valueTuple;
+                Center = (0, 0);
+            }
+        }
+
+        public bool TriangleIsIsosceles()
+        {
+            var sideOneLength = (decimal) Math.Sqrt(Math.Pow(Vertices[1].x-Vertices[0].x,2)+Math.Pow(Vertices[1].y-Vertices[0].y,2));
+            var sideTwoLength = (decimal) Math.Sqrt(Math.Pow(Vertices[2].x-Vertices[1].x,2)+Math.Pow(Vertices[2].y-Vertices[1].y,2));
+            var sideThreeLength = (decimal) Math.Sqrt(Math.Pow(Vertices[0].x-Vertices[2].x,2)+Math.Pow(Vertices[0].y-Vertices[2].y,2));
+            return (sideOneLength==sideTwoLength || sideOneLength==sideThreeLength)
+                   ||(sideTwoLength==sideOneLength || sideTwoLength==sideThreeLength)
+                   ||(sideThreeLength==sideOneLength || sideThreeLength==sideTwoLength);
+        }
+
+        public bool IsTriangleRight()
+        {
+            var sideOneLength = (decimal) Math.Sqrt(Math.Pow(Vertices[1].x-Vertices[0].x,2)+Math.Pow(Vertices[1].y-Vertices[0].y,2));
+            var sideTwoLength = (decimal) Math.Sqrt(Math.Pow(Vertices[2].x-Vertices[1].x,2)+Math.Pow(Vertices[2].y-Vertices[1].y,2));
+            var sideThreeLength = (decimal) Math.Sqrt(Math.Pow(Vertices[0].x-Vertices[2].x,2)+Math.Pow(Vertices[0].y-Vertices[2].y,2));
+            return (sideOneLength == sideTwoLength && sideOneLength == sideThreeLength);
+        } 
+    }
+
+    public class Polygon:FigureArea
+    {
+        public Polygon((double x, double y) center, List<(double x, double y)> vertices)
+        {
+            Center = center;
+            foreach (var v in vertices)
+            {
+                Vertices.Add(v);
+            }
+        }
+        public override double GetFigureArea()
+        {
+            double sum=0;
+            for (var i = 0; i < Vertices.Count-1; i++)
+            {
+                sum += Vertices[i].x * Vertices[i + 1].y - Vertices[i].y * Vertices[i + 1].x;
+            }
+            sum += Vertices[Vertices.Count-1].x * Vertices[0].y - Vertices[Vertices.Count-1].y * Vertices[0].x;
+            return Math.Abs(sum)/2 ;
+        }
+
+        public override void ToCenter()
+        {
+            for (var i = 0; i < Vertices.Count-1; i++)
+            {
+                var valueTuple = Vertices[i];
+                valueTuple.x = +Center.x;
+                valueTuple.y = +Center.y;
+                Vertices[i] = valueTuple;
+                Center = (0,0);
+                
+            }
+        }
+    }
+
+}
